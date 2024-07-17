b6cf16590b0d173fa92f4aea3256ed609c373828
Ganesh-Chougale <gchougale32@gmail.com>
2024-07-17 12:20:59 +0530

    Bubble Sort added

---
+++ Self/Selection Sort/bubbleSort.js
@@ -0,0 +1,16 @@
+let myArr = [98,12,87,23,76,34,65,56];
+
+let bubbleSort = (arr) => {
+    let swapped;
+    for (let i = 0; i < arr.length - 1; i++) {
+      for (let j = 0; j < arr.length - i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        }
+      }
+    }
+    return arr;
+  };
+  
+
+console.log(bubbleSort(myArr));
