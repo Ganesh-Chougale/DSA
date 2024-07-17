387b5ab1160d0c6e6f8a6a3f00a04983d8996340
Ganesh-Chougale <gchougale32@gmail.com>
2024-07-17 21:48:41 +0530

    Insertion SOrt Added

---
+++ Self/03 Insertion Sort/insertionSort.js
@@ -0,0 +1,17 @@
+let myArr = [23, 1, 10, 5, 2];
+
+function insertionSort(arr) {
+    for(let i = 1; i<arr.length; i++){
+        let key = arr[i];
+        let j = i-1;
+
+        while(arr[j]>key && j>=0){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+    }
+    return arr;
+}
+
+console.log(insertionSort(myArr));
