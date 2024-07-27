aeadb89e781d3d814673cffbd46ce3ec86e64503
Ganesh-Chougale <gchougale32@gmail.com>
2024-07-27 17:56:55 +0530

    27-july 6pm

--- Self/00 Miscellenoius/Recursion/factorialUsingRecur.js
+++ Self/00 Miscellenoius/Recursion/factorialUsingRecur.js
@@ -2,7 +2,7 @@ function factorial(num){
     if(num<2){
         return num;
     }else{
-        return num * fact(num-1);
+        return num * factorial(num - 1);
     }
 }
 console.log(factorial(5));
--- Self/04 Merge Sort/mergeSort.js
+++ Self/04 Merge Sort/mergeSort.js
@@ -1,24 +1,62 @@
-let myArr = [9,0,8,1,7,2,3,6,5,4];   
+let myArr = [9,0,8,1,7,2,3,6,5,4];
+
+// custom push() method
+function pusher(arr, item){
+    finalPosition = arr.length;
+    arr[finalPosition] = item;
+    return arr;
+}
+
+// custom shift() method
+function inValCut(arr){
+    let index = 0
+    const firstVal= arr[index];
+    while(index<arr.length){
+    arr[index] = arr[index+1];
+    index++;
+    }
+    arr.length = arr.length-1;
+        return firstVal;
+}
 
 function mergeSort(arr){
+    
     if(arr.length<2){
-        return arr;
+        return num
     }
     const mid = Math.floor(arr.length/2);
-    const leftArr = arr.slice(0, mid);
-    const rightArr = arr.slice(mid);
-    return merge(mergeSort(leftArr), mergeSort(rightArr));
+    //splice Starts
+    const leftArr = [];
+    for(let i = 0; i<mid; i++){
+        leftArr[i] = arr[i]
+    }
+    console.log("left Array :",leftArr);
+    
+    const rightArr = [];
+    let j = 0; // need new index because [i] will continue old index
+    for(let i=mid; i<arr.length; i++){
+        rightArr[j] = arr[i];
+        j++;
+    }
+    console.log("right Array :",rightArr);
+    // splice Ends
 
-    function merge(leftArr, rightArr){
-        const sortedArr = [];
-        while(leftArr.length && rightArr.length){
+    merge(mergeSort(leftArr), mergeSort(rightArr)); // recursive part
+    
+    
+    function merge(arr, leftArr, rightArr){
+        let sortedArr = [];
+        while(leftArr.length && right.length){
             if(leftArr[0] <= rightArr[0]){
-                sortedArr.push(leftArr.shift());
+                pusher(sortedArr, inValCut(leftArr));              
             }else{
-                sortedArr.push(rightArr.shift());
+                pusher(sortedArr, inValCut(rightArr));
             }
         }
-        return [...sortedArr, ...leftArr, ...rightArr]
     }
 }
-console.log(mergeSort(myArr));
+
+mergeSort(myArr);
+
+
+
